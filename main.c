cf7143e275a9403c56e449ca4c01a66c620a081e
dIronmanb <66776909+dIronmanb@users.noreply.github.com>
2020-07-17 11:49:06 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,71 +1,52 @@
-#include  <stdio.h>
+//sigmoid function
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#define NUM 5
+//double exp(double x) e의 x제곱을 구함.
 
-int main (void)
+void InputArray(double[], int size);
+void SigmoidArray(double [], int size);
+double SigmoidFunction(double x);
+void ShowArray(double[], int size);
+
+int main(void)
 {
-	int yy, mm, dd;
-	int dd1, dd10; //day 일의 자리와 십의 자리 표현
-	
-	printf("년월일을 차례대로 입력하세요 : ");
-	scanf("%d %d %d",&yy, &mm, &dd);
-
-	printf("\nDated this ");
-
-	
-    dd1 = dd % 10; dd10 = dd / 10;
-
-
-	switch (dd1)
-	{	
-		case 1 : if(dd10 != 1) printf("%dst ",dd);
-				 else printf("%dth ",dd); break;
-
-		case 2 : if(dd10 != 1) printf("%dnd ",dd);
-			     else printf("%dth ",dd); break;
-
-		case 3 : if(dd10 != 1)printf("%drd ",dd); 
-			     else printf("%dth ", dd); break;
-
-		default : printf("%dth ",dd); break;
-	}
-
-	printf("day of ");
-
-
-	switch (mm)
-	{
-		case 1 : printf("January"); break;
-		case 2 : printf("February"); break;
-		case 3 : printf("March"); break;
-		case 4 : printf("April"); break;
-		case 5 : printf("May"); break;
-		case 6 : printf("June"); break;
-		case 7 : printf("July"); break;
-		case 8 : printf("August"); break;
-		case 9 : printf("September"); break;
-		case 10 : printf("October"); break;
-		case 11 : printf("November"); break;
-		case 12 : printf("December"); break;
-	}
-
-	printf(", %d.\n",yy);
-
+	double a[NUM];
+	InputArray(a, NUM);
+	SigmoidArray(a, NUM);
+	ShowArray(a, NUM);
 	return 0;
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void InputArray(double a[], int size)
+{
+	printf("배열에 넣을 값을 지정하시오.\n");
+	for (int i = 0; i < size; i++)
+	{
+		printf("[%d] : ", i);
+		scanf("%lf", &a[i]);
+	}
+	printf("\n");
+}
+void ShowArray(double a[], int size)
+{
+	printf("배열 출력\n");
+	for (int i = 0; i < size; i++)
+	{
+		printf("a[%d] : %.6f\n", i, a[i]);
+	}
+	printf("\n");
+}
+void SigmoidArray(double a[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		a[i] = SigmoidFunction(a[i]);
+	}
+}
+double SigmoidFunction(double x)
+{
+	return 1 / (1 + exp(-x));
+}
